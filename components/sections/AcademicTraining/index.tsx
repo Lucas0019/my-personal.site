89729a48d2709c2bf9e1b6f93dd06f37f0bfbba8
Lucas Xavier <lxavier.00@outlook.com>
2026-01-04 14:01:22 -0300

    refactor: separate components into their specific folders

---
+++ components/sections/AcademicTraining/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { motion } from "motion/react";
+import { TRANSITION_SECTION, VARIANTS_SECTION } from "@/lib/constants";
+import { Spotlight } from "@/components/ui/spotlight";
+
+export const AcademicTraining = () => {
+  return (
+    <motion.section variants={VARIANTS_SECTION} transition={TRANSITION_SECTION}>
+      <h3 className="mb-5 text-lg font-medium">Formação Acadêmica</h3>
+      <div className="flex flex-col space-y-2">
+        <div className="relative overflow-hidden rounded-2xl bg-zinc-300/30 p-[1px] dark:bg-zinc-600/30">
+          <Spotlight
+            className="from-zinc-900 via-zinc-800 to-zinc-700 blur-2xl dark:from-zinc-100 dark:via-zinc-200 dark:to-zinc-50"
+            size={64}
+          />
+          <div className="relative h-full w-full rounded-[15px] bg-white p-4 dark:bg-zinc-950">
+            <div className="relative flex w-full flex-row justify-between">
+              <div>
+                <h4 className="font-normal dark:text-zinc-100">
+                  Fullstack Developer
+                </h4>
+                <p className="text-zinc-500 dark:text-zinc-400">JStack</p>
+              </div>
+              <p className="text-zinc-600 dark:text-zinc-400">
+                2023 - Presente
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="relative overflow-hidden rounded-2xl bg-zinc-300/30 p-[1px] dark:bg-zinc-600/30">
+          <Spotlight
+            className="from-zinc-900 via-zinc-800 to-zinc-700 blur-2xl dark:from-zinc-100 dark:via-zinc-200 dark:to-zinc-50"
+            size={64}
+          />
+          <div className="relative h-full w-full rounded-[15px] bg-white p-4 dark:bg-zinc-950">
+            <div className="relative flex w-full flex-row justify-between">
+              <div>
+                <h4 className="font-normal dark:text-zinc-100">
+                  CST - Análise e Desenvolvimento de Sistemas
+                </h4>
+                <p className="text-zinc-500 dark:text-zinc-400">
+                  FMU | FIAM-FAAM Faculdade Metropolitanas Unidas
+                </p>
+              </div>
+              <p className="text-zinc-600 dark:text-zinc-400">2018 - 2020</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
---
+++ components/sections/Introduction/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { motion } from "motion/react";
+import { TRANSITION_SECTION, VARIANTS_SECTION } from "@/lib/constants";
+
+export const Introduction = () => {
+  return (
+    <motion.section variants={VARIANTS_SECTION} transition={TRANSITION_SECTION}>
+      <div className="flex-1">
+        <h4>
+          Olá! Eu sou Lucas,
+          <br />
+          um desenvolvedor frontend há mais de 5 anos construindo experiências
+          que as pessoas realmente usam.
+        </h4>
+        <br />
+        <p className="text-zinc-600 dark:text-zinc-400">
+          Construindo aplicações web escaláveis e de alta performance.
+          Conectando design e desenvolvimento com código limpo e de fácil
+          manutenção, sempre buscando aprender novas tecnologias e aprimorar
+          minhas habilidades.
+        </p>
+      </div>
+    </motion.section>
+  );
+};
